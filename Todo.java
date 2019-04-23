70d5a3781d48fd031990b3b5bfbed7d01b3d30ad
Lowry West <4lowrywest@gmail.com>
2019-04-23 16:42:11 -0500

    Todo

---
+++ Todo.java
@@ -0,0 +1,37 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-04-23
+OU ID_______________: 113410037
+OU EMAIL____________: lowry.y.west-1@ou.edu
+
+Tasks:
+1. setupRegions method
+2. paintComponent method
+3. mouseClicked method
+4. setSelectedRegionContents method
+5. evaluate method
+6. GraphicalCalculatorFrame constructor
+7. setOperator action listener
+8. setOperand action listener
+
+Estimated Time (minutes):
+1. 15
+2. 20
+3. 20
+4. 20
+5. 20
+6. 10
+7. 15
+8. 15
+
+Actual Time (minutes):
+1. 15
+2. 15
+3. 15
+4. 20
+5. 10
+6. 10
+7. 20
+8. 10
+
+*/}
